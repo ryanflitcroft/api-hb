d029b72e166e5f1d446711c477470bba4267914d
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-02-15 17:10:34 -0800

    passing test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Pokemon/i);
+  const linkElement = screen.getByText(/Search Pokemon/i);
   expect(linkElement).toBeInTheDocument();
 });
