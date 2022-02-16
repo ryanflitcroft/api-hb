1fbcce0afff828645ab184b5528c7a6fdb47e2b4
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-02-16 09:27:38 -0800

    Component Weather called conditionally in WeatherSearch.

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 export default function Weather({ weather }) {
-  console.log('||weather: ', weather);
   return (
     <>
       <p>
--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -3,31 +3,27 @@ import Weather from './Weather';
 import WeatherSpinner from './WeatherSpinner';
 
 export default function WeatherSearch() {
-      // you'll need to track your weather search results, the loading state, and a form field for location with a default value.
+
   const [weather, setWeather] = useState([]);
   const [city, setCity] = useState('');
   const [state, setState] = useState('');
   const [country, setCountry] = useState('');
   const [loading, setLoading] = useState(false);
-  
+
   async function handleWeatherSubmit(e) {
     e.preventDefault();
     setLoading(true);
-        // set the loading state to true
-        // use fetch to make a request to your netlify weather function. Be sure to pass the location as a query param in the URL
     const response = await fetch(`/.netlify/functions/weather?city=${city}&state=${state}&country=${country}`);
-      
     const json = await response.json();
     setWeather(json);
-        // put the jsonified data in state and set the loading state to false
+    setLoading(false);
   }
-      
+
   return (
     <section className='weather'>
       {/* make the fetch on submit */}
       <form onSubmit={handleWeatherSubmit}>
             Search weather for a city
-        {/* add inputs/labels for city name, state, and country, using all the things we need with react forms. Don't forget to use the value property to sync these up with the default values in react state */}
         <label htmlFor='city'>City</label>
         <input name='city'
           onChange={(e) => setCity(e.target.value)}
@@ -42,16 +38,17 @@ export default function WeatherSearch() {
           value={country} />
         <button>Get weather</button>
       </form>
-      {/* Make a ForecastList component to import and use here. Use a ternary to display a loading spinner (make a <Spinner /> component for this) if the data is still loading. */}
-      
-      <Weather weather={weather}/>
+
+      {
+        weather.length !== 0
+        &&
+        <Weather weather={weather}/>
+      }
       {
         loading
         &&
         <WeatherSpinner />
       }
-
     </section>
   );
-
 }
