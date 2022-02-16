dca8cfa931128c2cea03518d922eab00eb6f6af4
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-02-15 17:04:51 -0800

    style changes

--- src/BusinessList.js
+++ src/BusinessList.js
@@ -3,11 +3,11 @@ import Business from './Business';
 
 export default function BusinessList({ businesses }) {
   return (
-    <>
+    <div>
       {businesses.map((business, i) =>
         <Business key={business.name + i}
           business={business} />
       )}
-    </>
+    </div>
   );
 }
--- src/PokemonList.js
+++ src/PokemonList.js
@@ -3,11 +3,11 @@ import Poke from './Poke';
 
 export default function PokemonList({ pokemon }) {
   return (
-    <>
+    <div>
       {pokemon.map((poke, i) =>
         <Poke key={poke + i}
           poke={poke} />
       )}
-    </>
+    </div>
   );
 }
