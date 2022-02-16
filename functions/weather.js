56449f42d448bed3ae0b788e64f9c1caa4e145e4
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-02-15 19:14:39 -0800

    rfc written for component WeatherSearch, server function weather. weather failing on const weatherResponse fetch to openweathermap api... will come back to it.

--- functions/weather.js
+++ functions/weather.js
@@ -1,14 +1,29 @@
-const request = require('superagent');
-
+const fetch = require('node-fetch');
 require('dotenv').config();
 
 exports.handler = async (event, context) => {
+
   try {
+    const { city, state, country } = event.queryStringParameters;
     // grab the city, state, and country from the request's query parameters
     // here is an example from the netlify docs:
     // https://functions.netlify.com/playground/#hello%2C-%7Bname%7D 
+    const geocodingResponse = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&limit=1&appid=${process.env.REACT_APP_OPENWEATHER_KEY}`);
 
-    // tragicly, we cannot just pass the city name to this API. it wants a latitude and longitude for the weather
+    const geocodingJson = await geocodingResponse.json();
+
+    const lat = geocodingJson[0].lat;
+    const lon = geocodingJson[0].lon;
+    console.log('---lat/lon', lat, lon);
+
+    //FIXME: fails here??
+    const weatherResponse = await fetch(`api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${process.env.REACT_APP_OPENWEATHER_KEY}`);
+
+    console.log('---weatherResponse', weatherResponse);
+
+    // const weatherJson = await weatherResponse.json();
+
+    // tragically, we cannot just pass the city name to this API. it wants a latitude and longitude for the weather
     // consult the yelp docs to figure out how to use a city, state, and country to make a request and get the latitude and longitude
     // https://openweathermap.org/api/geocoding-api
 
@@ -19,7 +34,7 @@ exports.handler = async (event, context) => {
     return { 
       statusCode: 200, 
     // this is where you shoot data back to the user. right now it's sending an empty object--replace this with the weather data. remember, you do need to stringify it, otherwise netlify gets mad. ¯\_(ツ)_/¯
-      body: JSON.stringify({}),
+      body: JSON.stringify(),
     };
   } catch (error) {
     console.log(error);
--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -1,27 +1,53 @@
 import { useState } from 'react';
+import WeatherSpinner from './WeatherSpinner';
 
 export default function WeatherSearch() {
       // you'll need to track your weather search results, the loading state, and a form field for location with a default value.
+  const [weather, setWeather] = useState([]);
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+  const [country, setCountry] = useState('');
+  const [loading, setLoading] = useState(false);
   
   async function handleWeatherSubmit(e) {
     e.preventDefault();
-      
+    setLoading(true);
         // set the loading state to true
         // use fetch to make a request to your netlify weather function. Be sure to pass the location as a query param in the URL
+    const response = await fetch(`/.netlify/functions/weather?city=${city}&state=${state}&country=${country}`);
       
-  
+    const json = await response.json();
+    setWeather(json);
         // put the jsonified data in state and set the loading state to false
   }
       
   return (
     <section className='weather'>
       {/* make the fetch on submit */}
-      <form>
+      <form onSubmit={handleWeatherSubmit}>
             Search weather for a city
         {/* add inputs/labels for city name, state, and country, using all the things we need with react forms. Don't forget to use the value property to sync these up with the default values in react state */}
+        <label htmlFor='city'>City</label>
+        <input name='city'
+          onChange={(e) => setCity(e.target.value)}
+          value={city} />
+        <label htmlFor='state'>State</label>
+        <input name='state'
+          onChange={(e) => setState(e.target.value)}
+          value ={state} />
+        <label htmlFor='country'>Country</label>
+        <input name='country'
+          onChange={(e) => setCountry(e.target.value)}
+          value={country} />
         <button>Get weather</button>
       </form>
-      {/* Make a ForecastList component to import and use here. Use a ternery to display a loading spinner (make a <Spinner /> component for this) if the data is still loading. */}
+      {/* Make a ForecastList component to import and use here. Use a ternary to display a loading spinner (make a <Spinner /> component for this) if the data is still loading. */}
+      {
+        loading
+        &&
+        <WeatherSpinner />
+      }
+
     </section>
   );
 
